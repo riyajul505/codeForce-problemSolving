7f06c6b950257d86b4b27c96830963bf27d827a6
riyajul505 <rlriyajul@gmail.com>
2025-04-15 23:40:07 +0600

    ali baba and puzzle solved

--- D_Ali_Baba_and_Puzzles.c
+++ D_Ali_Baba_and_Puzzles.c
@@ -2,6 +2,37 @@
 
 int main(){
 
+    long long int a;
+    long long int b;
+    long long int c;
+    long long int d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+    
+    if (a+b-c == d)
+    {
+        printf("YES");
+    }
+    else if (a-b+c == d){
+        printf("YES");
+    }
+    else if (a*b-c == d){
+        printf("YES");
+    }
+    else if (a-b*c == d){
+        printf("YES");
+    }
+    else if (a+b*c == d){
+        printf("YES");
+    }
+    else if (a*b+c == d){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    
+    
+    
     
     return 0;
 }
