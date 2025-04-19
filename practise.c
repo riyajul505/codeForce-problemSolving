7b08b78c66ef5edd6bbefa84fb291167bea829ea
riyajul505 <rlriyajul@gmail.com>
2025-04-19 20:53:06 +0600

    Adjust array for newer value

--- practise.c
+++ practise.c
@@ -2,13 +2,41 @@
 
 int main()
 {
-    for (int i = 0; i <= 10; i++)
-    {
-        if(i>2){
-            continue;
-        }
-        printf("hi");
+    // taking input from user for array size
+    int n;
+    scanf("%d", &n);
+    // array declaration
+    int arr[n+1];
+
+    // taking input on array
+    for(int i = 0; i<n; i++){
+        scanf("%d", &arr[i]);
     }
+
+    // taking input for index and value 
+    int index, value;
+    scanf("%d %d", &index, &value);
+
+    // shifting elements to make space for the new value
+    int arraySize = n;
+    while (arraySize > index)
+    {
+        arr[arraySize] = arr[arraySize-1];
+        arraySize--;
+    }
+    
+    // placing the value at the shifted index
+    arr[index] = value;
+
+    // printing the updated array
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    
+    
+    
     
 
     return 0;
